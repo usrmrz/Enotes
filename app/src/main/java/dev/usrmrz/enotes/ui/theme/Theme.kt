2857c18195f67fecc5d6e2c4c131fbf21e4d419e
usrmrz <usrmrz@gmail.com>
2024-09-13 00:26:42 +0300

    edit ui.theme components. The project setup is complite, so it's time to begin to form directory structure

--- app/src/main/java/dev/usrmrz/enotes/ui/theme/Theme.kt
+++ app/src/main/java/dev/usrmrz/enotes/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package dev.usrmrz.enotes.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.White,
+    secondary = BabyBlue,
+    tertiary = Violet,
+    background = RedOrange,
+    onBackground = RedPink,
+    surface = LightGreen
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,7 +50,6 @@ fun EnotesTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
@@ -53,6 +57,7 @@ fun EnotesTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
