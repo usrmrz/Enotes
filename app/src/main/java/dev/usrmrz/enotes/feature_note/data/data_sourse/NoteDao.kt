c8f40ded6facd6109c349da302971e3a39dc78cc
usrmrz <usrmrz@gmail.com>
2024-09-13 23:29:17 +0300

    create dao interface

---
+++ app/src/main/java/dev/usrmrz/enotes/feature_note/data/data_sourse/NoteDao.kt
@@ -0,0 +1,25 @@
+package dev.usrmrz.enotes.feature_note.data.data_sourse
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import dev.usrmrz.enotes.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query(value = "SELECT * FROM note")
+    fun getNotes(): Flow<List<Note>>
+
+    @Query(value = "SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteById(id: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
