edadae6301ab1444bc54e20b78d4978ccbbda84a
usrmrz <usrmrz@gmail.com>
2024-09-15 17:02:53 +0300

    add two more fun for provide some singletones

--- app/src/main/java/dev/usrmrz/enotes/di/AppModule.kt
+++ app/src/main/java/dev/usrmrz/enotes/di/AppModule.kt
@@ -6,8 +6,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.usrmrz.enotes.NoteApp
 import dev.usrmrz.enotes.feature_note.data.data_sourse.NoteDatabase
+import dev.usrmrz.enotes.feature_note.data.repository.NoteRepositoryImpl
+import dev.usrmrz.enotes.feature_note.domain.repository.NoteRepository
+import dev.usrmrz.enotes.feature_note.domain.use_case.DeleteNote
+import dev.usrmrz.enotes.feature_note.domain.use_case.GetNotes
+import dev.usrmrz.enotes.feature_note.domain.use_case.NoteUseCases
 import javax.inject.Singleton
 
 @Module
@@ -22,5 +26,20 @@ object AppModule {
             NoteDatabase::class.java,
             NoteDatabase.DATABASE_NAME
         ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
     }
 }
--- app/src/main/java/dev/usrmrz/enotes/feature_note/data/data_sourse/NoteDatabase.kt
+++ app/src/main/java/dev/usrmrz/enotes/feature_note/data/data_sourse/NoteDatabase.kt
@@ -1,10 +1,7 @@
 package dev.usrmrz.enotes.feature_note.data.data_sourse
 
-//import android.content.Context
 import androidx.room.Database
-//import androidx.room.Room
 import androidx.room.RoomDatabase
-import dev.usrmrz.enotes.NoteApp
 import dev.usrmrz.enotes.feature_note.domain.model.Note
 
 @Database(
