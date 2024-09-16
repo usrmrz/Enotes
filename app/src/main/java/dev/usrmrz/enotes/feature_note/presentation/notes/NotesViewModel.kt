6b2b9b00b80d9f8d1ed24087192f3142f59a32f7
usrmrz <usrmrz@gmail.com>
2024-09-16 03:56:21 +0300

    NotesViewModel is filled with content

--- app/src/main/java/dev/usrmrz/enotes/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/dev/usrmrz/enotes/feature_note/presentation/notes/NotesViewModel.kt
@@ -1,11 +1,46 @@
 package dev.usrmrz.enotes.feature_note.presentation.notes
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.usrmrz.enotes.feature_note.domain.model.Note
 import dev.usrmrz.enotes.feature_note.domain.use_case.NoteUseCases
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NotesViewModel @Inject constructor(
     private val noteUseCases: NoteUseCases
-) : ViewModel() { }
+) : ViewModel() {
+
+    private val _state = mutableStateOf(NotesState())
+    val state: State<NotesState> = _state
+
+    private var recentlyDeletedNote: Note? = null
+
+    fun onEvent(event: NotesEvent) {
+        when(event) {
+            is NotesEvent.Order -> {
+                TODO()
+            }
+            is NotesEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    noteUseCases.deleteNote(event.note)
+                    recentlyDeletedNote = event.note
+                }
+            }
+            NotesEvent.RestoreNote -> {
+                viewModelScope.launch {
+
+                }
+            }
+            NotesEvent.ToggleOrderSection -> {
+                _state.value = state.value.copy(
+                    isOrderSectionVisible = !state.value.isOrderSectionVisible
+                )
+            }
+        }
+    }
+}
