f79f99b19c86a1838defe549726b4dc51005ae4c
usrmrz <usrmrz@gmail.com>
2024-09-19 03:24:37 +0300

    begin with user interface creating default radio button and order section

---
+++ app/src/main/java/dev/usrmrz/enotes/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,33 @@
+package dev.usrmrz.enotes.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.usrmrz.enotes.feature_note.domain.util.NoteOrder
+import dev.usrmrz.enotes.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = {  }
+            )
+
+        }
+
+    }
+
+}
