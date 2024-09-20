c8df5fa1417e21b785260bc816db97e1d23011f8
usrmrz <usrmrz@gmail.com>
2024-09-21 00:23:21 +0300

    builded again. commented lines with LazyColumn body

--- app/src/main/java/dev/usrmrz/enotes/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/dev/usrmrz/enotes/feature_note/presentation/notes/NotesScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Sort
 import androidx.compose.material3.FloatingActionButton
@@ -26,7 +26,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -38,7 +37,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import dev.usrmrz.enotes.feature_note.presentation.notes.components.NoteItem
 import dev.usrmrz.enotes.feature_note.presentation.notes.components.OrderSection
-import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -66,7 +64,7 @@ fun NotesScreen(
                 )
             }
         },
-        scaffoldState = scaffoldState
+//        scaffoldState = scaffoldState
     ) {
         Column(
             modifier = Modifier
@@ -88,7 +86,7 @@ fun NotesScreen(
                     },
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Sort,
+                        imageVector = Icons.AutoMirrored.Filled.Sort,
                         contentDescription = "Sort"
                     )
                 }
@@ -112,31 +110,31 @@ fun NotesScreen(
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(state.notes) { note ->
-                    NoteItem(
-                        note = note,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-
-
-                            },
-                        onDeleteClick = {
-                            viewModel.onEvent(NotesEvent.DeleteNote(note))
-                            scope.launch {
-                                val result = scaffoldState.snackbarHostState.showSnackBar(
-                                    message = "Not deleted",
-                                    actionLabel = "Undo"
-                                )
-                                if (result == SnackbarResult.ActionPerformed) {
-                                    viewModel.onEvent(NotesEvent.RestoreNote)
-                                }
-                            }
-                        }
-
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
+//                items(note) { note ->
+//                    NoteItem(
+//                        note = note,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .clickable {
+//
+//
+//                            },
+//                        onDeleteClick = {
+//                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+//                            scope.launch {
+//                                val result = scaffoldState.snackbarHostState.showSnackBar(
+//                                    message = "Not deleted",
+//                                    actionLabel = "Undo"
+//                                )
+//                                if (result == SnackbarResult.ActionPerformed) {
+//                                    viewModel.onEvent(NotesEvent.RestoreNote)
+//                                }
+//                            }
+//                        }
+//
+//                    )
+//                    Spacer(modifier = Modifier.height(16.dp))
+//                }
             }
         }
     }
